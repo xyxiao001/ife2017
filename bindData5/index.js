b8c72b7619b85f4f4fba0666108b23e9d0422185
xyxiao001 <463618622@qq.com>
2017-02-26 00:09:12 +0800

    add bind5

--- bindData5/index.js
+++ bindData5/index.js
@@ -3,19 +3,64 @@
 function Vue(data) {
   this.el = data.el
   this.data = data.data
+  // 绑定数据
+  this.observer(this.data)
   // 渲染模板
+  this.getMode()
   this.render()
+}
+
+Vue.prototype.observer = function (obj) {
+  // 设置值
+  let i, v
+  for (i in obj) {
+    if (obj.hasOwnProperty(i)) {
+      v = obj[i]
+
+      // 如果是对象 则重新设置
+      if (typeof v === 'object') {
+        v = this.observer(v)
+      }
+
+      // 绑定对象get set
+      this.bindVal(obj, i, v)
+    }
+  }
+  return obj
+}
+
+Vue.prototype.bindVal = function (obj, i, v) {
+  let that = this
+  Object.defineProperty(obj, i, {
+    // 是否可以被循环枚举出来
+    enumerable: true,
+    // 是否可配置
+    configurable: true,
+    get: function () {
+      return v
+    },
+    set: function (newVal) {
+      console.log('我要重新渲染了..')
+      if (typeof newVal === 'object') {
+        newVal = this.observer(newVal)
+      }
+      if (newVal !== v) {
+        v = newVal
+      }
+      that.render()
+    }
+  })
 }
 
 // 获取需要渲染的模板
 Vue.prototype.getMode = function () {
   this.dom = document.querySelector(this.el)
-  return this.dom.innerHTML
+  this.template = this.dom.innerHTML
 }
 
 // 渲染函数
 Vue.prototype.render = function () {
-  var html = this.getMode()
+  var html = this.template
   var pattern = /\{\{(.*?)\}\}/g
   var that = this
   html.match(pattern).forEach(function (val) {
