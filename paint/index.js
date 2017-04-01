38f0a798e9b6b87633a9842eb416ade992857079
xyxiao001 <463618622@qq.com>
2017-04-01 22:27:33 +0800

    添加平滑的连线

--- paint/index.js
+++ paint/index.js
@@ -1,11 +1,16 @@
 'use strict'
 var options = {
   color: '#00000',
+  lastX: -1,
+  lastY: -1,
 }
 
 // 获取画布节点
 var paint = document.getElementById('paint')
 var ctx = paint.getContext('2d')
+ctx.strokeStyle= '#000'
+ctx.lineWidth = 2
+ctx.lineCap = 'round'
 
 // 开始画画
 paint.addEventListener('mousedown', function () {
@@ -15,6 +20,8 @@ paint.addEventListener('mousedown', function () {
 
 paint.addEventListener('mouseup', function () {
   console.log('end')
+  options.lastX = -1
+  options.lastY = -1
   paint.removeEventListener('mousemove', move)
 })
 
@@ -28,12 +35,15 @@ function move(e) {
 
 // 画图函数
 function draw(x, y) {
-  ctx.strokeStyle= '#000'
-  ctx.lineWidth = 1
-  ctx.lineCap= 'round'
   ctx.beginPath()
-  ctx.moveTo(x, y)
+  if (options.lastX > 0) {
+    ctx.moveTo(options.lastX, options.lastY)
+  } else {
+    ctx.moveTo(x, y)
+  }
+  options.lastX = x
+  options.lastY = y
   ctx.lineTo(x, y)
-  ctx.stroke()
   ctx.closePath()
+  ctx.stroke()
 }
