6914cdcf9874c49f32f30c86dc7ef2d28940d695
xyxiao <463618622@qq.com>
2017-04-05 15:30:41 +0800

    添加导出

--- paint/index.js
+++ paint/index.js
@@ -1,9 +1,13 @@
 'use strict'
 var options = {
   color: '#00000',
+  width: 1000,
+  height: 500,
   lineWidth: 2,
   lastX: -1,
   lastY: -1,
+  now: 0,
+  history: []
 }
 
 // 获取画布节点
@@ -16,6 +20,8 @@ updatePen()
 // 开始画画
 paint.addEventListener('mousedown', function () {
   console.log('start')
+  options.now += 1
+  options.history.push([])
   paint.addEventListener('mousemove', move)
   paint.addEventListener('mouseleave', end)
 })
@@ -24,11 +30,26 @@ paint.addEventListener('mouseup', end)
 
 // 清除画布
 document.querySelector('#clear').addEventListener('click', function () {
-  clear(0, 0, 1000, 500)
+  clear(0, 0, options.width, options.height)
+  options.now = 0
+  options.history = []
 })
 
 // 导出图片
 document.querySelector('#report').addEventListener('click', report)
+
+// 上一步
+document.querySelector('#pre').addEventListener('click', function () {
+  var arr = options.history
+  if (arr.length > 0) {
+    arr.pop()
+    options.now -= 1
+    updateCanvas()
+  } else {
+    console.log(options.history)
+    console.log('已经回退完了')
+  }
+})
 
 function end() {
   console.log('end')
@@ -41,7 +62,11 @@ function end() {
 function move(e) {
   var x = e.clientX
   var y = e.clientY
-  draw(x, y)
+  options.history[options.now - 1].push({
+    x: x,
+    y: y
+  })
+  updateCanvas()
   // console.log('正在移动')
 }
 
@@ -73,4 +98,22 @@ function clear(x, y, w, h) {
 function report() {
 	window.open(paint.toDataURL())
   console.log('导出图片')
+}
+
+// 更新视图
+function updateCanvas() {
+  // 先清除画布
+  clear(0, 0, options.width, options.height)
+  // 循环历史命令
+  options.history.forEach(function(val, index) {
+    ctx.beginPath()
+    ctx.moveTo(val[0].x, val[0].y)
+    for (var i = 1; i < val.length - 2; i++) {
+      var c = (val[i].x + val[i + 1].x) / 2;
+      var d = (val[i].y + val[i + 1].y) / 2;
+      ctx.quadraticCurveTo(val[i].x, val[i].y, c, d)
+    }
+    // ctx.quadraticCurveTo(val[i].x, val[i].y, val[i + 1].x, val[i + 1].y)
+    ctx.stroke()
+  })
 }
